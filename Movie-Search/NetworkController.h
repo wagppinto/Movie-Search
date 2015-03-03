8142b82aeb64f3c4e1531742a51684e28009ef72
wagppinto <wagpinto@me.com>
2015-03-03 15:31:54 -0700

    Network Controller added to the project - No API KEY

---
+++ Movie-Search/NetworkController.h
@@ -0,0 +1,13 @@
+//
+//  NetworkController.h
+//  Movie-Search
+//
+//  Created by Wagner Pinto on 3/3/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NetworkController : NSObject
+
+@end
---
+++ Movie-Search/NetworkController.m
@@ -0,0 +1,13 @@
+//
+//  NetworkController.m
+//  Movie-Search
+//
+//  Created by Wagner Pinto on 3/3/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "NetworkController.h"
+
+@implementation NetworkController
+
+@end
